4f16c70faf99727ffb966ff562cf394f0770c424
Henry Stoke <github@hmstoke.com>
2022-03-06 12:10:51 +0900

    Use RTK typed selector

--- src/state/shared/hooks.ts
+++ src/state/shared/hooks.ts
@@ -1,14 +1,11 @@
 import { useCallback } from "react";
-import { useSelector as useSelectorRaw } from "react-redux";
+import { TypedUseSelectorHook, useSelector as useSelectorRaw } from "react-redux";
 import { TopHatState } from "..";
 import { changeCurrencyValue } from "../data";
 import { useCurrencyMap, useDefaultCurrency } from "../data/hooks";
 import { ID, SDate } from "./values";
 
-export const useSelector = useSelectorRaw as <T>(
-    selector: (state: TopHatState) => T,
-    equalityFn?: ((left: T, right: T) => boolean) | undefined
-) => T;
+export const useSelector: TypedUseSelectorHook<TopHatState> = useSelectorRaw;
 
 export const useLocaliseCurrencies = () => {
     const userDefaultCurrency = useDefaultCurrency();
